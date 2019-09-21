c220ca40bd22a42e42a92be1a0709d899899b876
konoui <django.thon.py@gmail.com>
2019-09-21 22:28:33 +0900

    update readonly variable and run as shellscript

--- cni_plugin_sequence.sh
+++ cni_plugin_sequence.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -euv
 
-export IMDS_ENDPOINT="http://169.254.169.254/latest/meta-data"
-export PRIMARY_ENI_MAC_ADDRESS=$(curl -s ${IMDS_ENDPOINT}/mac)
-export SUBNET_ID=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/${PRIMARY_ENI_MAC_ADDRESS}/subnet-id)
-export SECURITY_GROUP_ID="$(curl -s ${IMDS_ENDPOINT}/macs/${PRIMARY_ENI_MAC_ADDRESS}/security-group-ids)"
+readonly IMDS_ENDPOINT="http://169.254.169.254/latest/meta-data"
+readonly PRIMARY_ENI_MAC_ADDRESS=$(curl -s ${IMDS_ENDPOINT}/mac)
+readonly SUBNET_ID=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/${PRIMARY_ENI_MAC_ADDRESS}/subnet-id)
+readonly SECURITY_GROUP_ID="$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/${PRIMARY_ENI_MAC_ADDRESS}/security-group-ids)"
 export AWS_DEFAULT_REGION=$(curl -s ${IMDS_ENDPOINT}/placement/availability-zone | sed -e 's/.$//')
-export INSTANCE_ID=$(curl -s ${IMDS_ENDPOINT}/instance-id)
-
+readonly INSTANCE_ID=$(curl -s ${IMDS_ENDPOINT}/instance-id)
 
 ## Utils
+get_device_by_hwaddr () {
+    LANG=C ip -o link | awk -F ': ' -vIGNORECASE=1 '!/link\/ieee802\.11/ && /'"$1"'/ { print $2 }'
+}
+
 cidr2mask() {
   local i mask=""
   local full_octets=$(($1/8))
@@ -36,6 +39,7 @@ prepare_task_eni () {
         --subnet-id ${SUBNET_ID} \
         --groups ${SECURITY_GROUP_ID})
     local NETWORK_INTERFACE_ID=$(echo ${NETWORK_INTERFACE_INFO} | jq -r '.NetworkInterface.NetworkInterfaceId')
+    readonly MAC_ADDRESS=$(echo ${NETWORK_INTERFACE_INFO} | jq -r '.NetworkInterface.MacAddress')
     echo create eni: ${NETWORK_INTERFACE_ID}
 
     local DEVICE_INDEX=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} \
@@ -51,7 +55,7 @@ prepare_task_eni () {
 ## Run pause container
 run_pause_container () {
     local PAUSE_IMAGE="kubernetes/pause"
-    PAUSE_CONTAINER_ID=$(docker run -d --net=none ${PAUSE_IMAGE})
+    readonly PAUSE_CONTAINER_ID=$(docker run -d --net=none ${PAUSE_IMAGE})
     echo "pause image: ${PAUSE_IMAGE}, pause container id: ${PAUSE_CONTAINER_ID}"
 }
 
@@ -60,44 +64,45 @@ run_pause_container () {
 ## see https://github.com/aws/amazon-ecs-agent/blob/8b5e18630375fc2ff656704d6228edfe4d412048/proposals/eni.md
 ### i. Get MAC Address for the ENI from EC2 Instance Metadata Service
 get_mac_address_from_metadata () {
-    # see https://github.com/aws/amazon-ecs-agent/blob/d2456beb048d36bfe18159ad7f35ca6b78bb9ee9/misc/eni-trunking-validator/eni-trunking-validator.go#L174
-     MAC_ADDRESS=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/ | head -1 | sed 's|/||g')
+    # FIXME confirm mac address of a attached eth<N>
+    #readonly MAC_ADDRESS=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/ | head -1 | sed 's|/||g')
+    # Note: Omitted getting from metadata
     echo mac address: ${MAC_ADDRESS}
 }
 
 ### ii. Get ENI device name on default namespace
 get_device_name_by_mac_address () {
-    source /etc/sysconfig/network-scripts/network-functions
-    DEVICE_NAME=$(get_device_by_hwaddr ${MAC_ADDRESS})
+    # see source /etc/sysconfig/network-scripts/network-functions
+    readonly DEVICE_NAME=$(get_device_by_hwaddr ${MAC_ADDRESS})
     echo device name: ${DEVICE_NAME}
 }
 
 ### iii. Get network gateway mask
 get_gateway_info () {
     # e.g SUBNET_CIDR is 10.0.0.0/20
-    SUBNET_CIDR=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/${MAC_ADDRESS}/subnet-ipv4-cidr-block)
+    readonly SUBNET_CIDR=$(curl -s ${IMDS_ENDPOINT}/network/interfaces/macs/${MAC_ADDRESS}/subnet-ipv4-cidr-block)
     # CIDR is 20
-    CIDR=$(echo ${SUBNET_CIDR} | grep -oP "(?<=/)[0-9]+")
+    readonly CIDR=$(echo ${SUBNET_CIDR} | grep -oP "(?<=/)[0-9]+")
     # GATEWAY_MASK is 255.255.240.0
-    GATEWAY_MASK=$(cidr2mask ${CIDR})
+    readonly GATEWAY_MASK=$(cidr2mask ${CIDR})
     # LOCAL_NETWORK is 10.0.0.0
     local LOCAL_NETWORK=$(echo ${SUBNET_CIDR} | grep -oP "[^/]+?(?=/)")
     # VPC_ROUTER is 10.0.0.1
-    VPC_ROUTER=$(echo ${LOCAL_NETWORK} | sed -r "s|(.*)([0-9]+)|\11|")
+    readonly VPC_ROUTER=$(echo ${LOCAL_NETWORK} | sed -r "s|(.*)([0-9]+)|\11|")
     echo "subnet cidir: ${SUBNET_CIDR} => gateway mask: ${GATEWAY_MASK}"
     echo local network: ${LOCAL_NETWORK}, vpc router: ${VPC_ROUTER}
 }
 
 ### iv. Get Primary IP Address of the ENI
 get_primary_ip_address () {
-     PRIMARY_IP_ADDRESS=$(ip -4 addr show ${DEVICE_NAME} | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-     echo device name: ${DEVICE_NAME}, primariy ip address: ${PRIMARY_IP_ADDRESS}
+    readonly PRIMARY_IP_ADDRESS=$(ip -4 addr show ${DEVICE_NAME} | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    echo device name: ${DEVICE_NAME}, primariy ip address: ${PRIMARY_IP_ADDRESS}
 }
 
 ### v. Move the ENI to container's namespace
 hack_pause_network_namespace () {
-    PAUSE_NETNS="${PAUSE_CONTAINER_ID:0:12}-pause-container-ns"
-    PAUSE_PID=$(docker inspect ${PAUSE_CONTAINER_ID} --format '{{.State.Pid}}')
+    readonly PAUSE_NETNS="${PAUSE_CONTAINER_ID:0:12}-pause-container-ns"
+    readonly PAUSE_PID=$(docker inspect ${PAUSE_CONTAINER_ID} --format '{{.State.Pid}}')
     # create symbolic link to /var/run/netns/ as we should manage the pause container namespace by ip command    
     # see http://man7.org/linux/man-pages/man8/ip-netns.8.html
     sudo mkdir -p /var/run/netns
@@ -107,7 +112,9 @@ hack_pause_network_namespace () {
 }
 
 move_eni_to_pause_network_namespace () {
-    DEVICE_NAME_ON_PAUSE_NETNS=${DEVICE_NAME}
+    readonly DEVICE_NAME_ON_PAUSE_NETNS=eth1
+    # rename device name eth<N> to eth1 on pause netns
+    ip netns exec hoge ip link set ${DEVICE_NAME} name ${DEVICE_NAME_ON_PAUSE_NETNS}
     sudo ip link set ${DEVICE_NAME} netns ${PAUSE_PID}
     sudo ip netns exec ${PAUSE_NETNS} ip link set ${DEVICE_NAME_ON_PAUSE_NETNS} up
     echo device name on pause netns: 
@@ -124,7 +131,6 @@ reassign_primary_ip_address_to_eni () {
 ### vii. Setup route to internet via the gateway
 setup_route () {
     sudo ip netns exec ${PAUSE_NETNS} ip route add default via ${VPC_ROUTER} dev ${DEVICE_NAME_ON_PAUSE_NETNS}
-    # sudo ip netns exec ${PAUSE_NETNS} ip route add ${SUBNET_CIDR} via 0.0.0.0 dev ${DEVICE_NAME_ON_PAUSE_NETNS}
     echo routes on pause netns: 
     sudo ip netns exec ${PAUSE_NETNS} ip route show
 }
@@ -150,3 +156,5 @@ main () {
     run_pause_container
     assign_eni_to_pause_netns
 }
+
+main
